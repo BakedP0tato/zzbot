d465fbdd5a1c42df2b2b620f91488cbd4fb19899
PizzaMan <ycooipizza@gmail.com>
2021-11-03 14:30:00 +0800

    bug fix

--- cogs/fun.py
+++ cogs/fun.py
@@ -33,7 +33,7 @@ class Fun(commands.Cog):
             await ctx.send(f'Try to guess the number : {lowest} to {highest} (Any non-integer to quit)')
             msg = await self.bot.wait_for(event='message', check=check(ctx), timeout=60)
             try:
-                guess = int(round(msg.content))
+                guess = int(msg.content)
             except ValueError:
                 await ctx.send('Game quitted')
                 break
@@ -95,7 +95,7 @@ class Fun(commands.Cog):
             'Suppose that you were sitting down at a table. The napkins are in front of you, which napkin would you take? The one on your ‘left’? Or the one on your ‘right’? The one on your left side? Or the one on your right side? Usually you would take the one on your left side. That is ‘correct’ too. But in a larger sense on society, that is wrong. Perhaps I could even substitute ‘society’ with the ‘Universe’. The correct answer is that ‘It is determined by the one who takes his or her own napkin first.’ …Yes? If the first one takes the napkin to their right, then there’s no choice but for others to also take the ‘right’ napkin. The same goes for the left. Everyone else will take the napkin to their left, because they have no other option. This is ‘society’… Who are the ones that determine the price of land first? There must have been someone who determined the value of money, first. The size of the rails on a train track? The magnitude of electricity? Laws and Regulations? Who was the first to determine these things? Did we all do it, because this is a Republic? Or was it Arbitrary? NO! The one who took the napkin first determined all of these things! The rules of this world are determined by that same principle of ‘right or left?’! In a Society like this table, a state of equilibrium, once one makes the first move, everyone must follow! In every era, this World has been operating by this napkin principle. And the one who ‘takes the napkin first’ must be someone who is respected by all. It’s not that anyone can fulfill this role… Those that are despotic or unworthy will be scorned. And those are the ‘losers’. In the case of this table, the ‘eldest’ or the ‘Master of the party’ will take the napkin first… Because everyone ‘respects’ those individuals.',
         ]
         try:
-            choice = int(round(choice)) - 1
+            choice = int(choice) - 1
         except ValueError:
             await ctx.send("Must be a number")
         if 0 <= choice < len(jojoquotes):
@@ -117,6 +117,10 @@ class Fun(commands.Cog):
             await ctx.send('say goodbye')
             await asyncio.sleep(1)
         await ctx.send(response.format(member))
+
+    @commands.command(help='get zzed')
+    async def zz(self, ctx, *, member: discord.Member):
+        await ctx.send(f'get zzed {member.mention} ')
         
 def setup(bot): # a extension must have a setup function
 	bot.add_cog(Fun(bot))
